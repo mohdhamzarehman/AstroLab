b765fde9cf2be1a149c3ae222addd326b306d760
Mohammad Hamza <134505680+mohdhamzarehman@users.noreply.github.com>
2023-08-26 09:54:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// (function(){
+//     let socials = document.querySelectorAll('.social div')
+
+//     socials.forEach(function(social,index){
+//         social,style,animation ='move .6s ease-in-out forwards s(index/7*.2)'
+//     })
+// }() )
